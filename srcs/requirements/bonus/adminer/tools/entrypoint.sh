f577372a9ba53157e8a6085b11533864625d5e4d
soramon0 <23435312+soramon0@users.noreply.github.com>
2026-08-05 23:04:38 +0100

    Refactor Dockerfiles for Adminer, cAdvisor, and Redis by removing entrypoint scripts
    - Updated the Dockerfiles to set the respective services (Adminer, cAdvisor, Redis) as the entrypoint directly, eliminating the need for separate entrypoint scripts.
    - This change simplifies the container startup process and reduces unnecessary files, enhancing overall efficiency.

--- srcs/requirements/bonus/adminer/tools/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-set -e
-exec php -S 0.0.0.0:8080 -t /var/www/adminer
--- srcs/requirements/bonus/cadvisor/tools/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-exec /usr/local/bin/cadvisor \
-	--port=8080 \
-	--docker_only=true \
-	--housekeeping_interval=30s
